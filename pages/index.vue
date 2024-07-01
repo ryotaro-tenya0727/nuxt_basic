7b8c470096e3b31a4afeebc16f129279230dd863
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-07-01 21:49:38 +0900

    ADD: axios追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage index</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
+  <div class="container">
+    <div>
+      <h1 class="title">Zenn-app</h1>
+      <h2 class="subtitle">
+        {{ subTitle }}
+      </h2>
+      <button @click="getSomething">タスク取得</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      subTitle: 'Zenn is good service!!',
+    };
+  },
+  methods: {
+    getSomething() {
+      alert('Get Something!!');
+    },
+  },
+};
+</script>
